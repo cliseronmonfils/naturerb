c9e00018d6ab114840e835fb68c3fe157111a74d
Carol-Anne <liseron.carolanne@gmail.com>
2020-01-29 15:55:05 +0100

    Nouveau logo + Nouvelles fonctionnalités

--- app/src/main/java/com/example/naturerb/MainActivity.java
+++ app/src/main/java/com/example/naturerb/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.naturerb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog() {
+        Dialog dialog = new Dialog();
+        dialog.show(getSupportFragmentManager(), "dialog");
+
+
     }
 }
